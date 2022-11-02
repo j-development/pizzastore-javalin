2a401ab04edd337e2a4a717841607c499404e3d5
ekdahl1 <johan.ekdahl96@gmail.com>
2022-11-02 11:31:52 +0100

    PizzaRepositoryTest deletePizzaByIdSuccessful test red

--- src/test/java/org/ec/Pizza/PizzaRepositoryTest.java
+++ src/test/java/org/ec/Pizza/PizzaRepositoryTest.java
@@ -1,4 +1,5 @@
 package org.ec.Pizza;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,12 +10,15 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
+import static org.mockito.Mockito.doNothing;
+
+
 @ExtendWith(MockitoExtension.class)
 public class PizzaRepositoryTest {
 
 	@Mock
 	PizzaRepository classUnderTest;
-	List<Pizza>	pizzas;
+	List<Pizza> pizzas;
 
 	@BeforeEach
 	void setUp() {
@@ -35,5 +39,20 @@ public class PizzaRepositoryTest {
 		Assertions.assertEquals(result, pizzas);
 	}
 
+	@Test
+	void deletePizzaByIdSuccessful() {
+		//given
+		Pizza pizza1 = new Pizza(1, "VESUVIO", 100, 1);
+		classUnderTest.add(pizza1);
+		int pizzaId = 1;
 
+
+		//when
+		classUnderTest.deleteById(pizzaId);
+		doNothing().when(classUnderTest).deleteById(pizzaId);
+
+
+		//then
+		Mockito.verify(classUnderTest, Mockito.times(2)).deleteById(pizzaId);
+	}
 }
