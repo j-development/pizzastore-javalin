157c33fdd56d1fa2b9d43ae5b02925680872faae
ekdahl1 <johan.ekdahl96@gmail.com>
2022-11-02 14:31:20 +0100

    PizzaTest succesfullyCreatePizzaAndTestConstructor test green

--- src/test/java/org/ec/Pizza/PizzaTest.java
+++ src/test/java/org/ec/Pizza/PizzaTest.java
@@ -9,6 +9,9 @@ public class PizzaTest {
 	void succesfullyCreatePizzaAndTestConstructor() {
 		Pizza pizza1 = new Pizza(1, "VESUVIO", 100, 1);
 
-		assertEquals(2, pizza1.getId());
+		assertEquals(1, pizza1.getId());
+		assertEquals("VESUVIO", pizza1.getName());
+		assertEquals(100, pizza1.getPrice());
+		assertEquals(1, pizza1.getPizzagroup());
 	}
 }
