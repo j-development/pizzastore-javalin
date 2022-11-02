db2dc9ed5ed70652467776b9507db2b100be39f2
ekdahl1 <johan.ekdahl96@gmail.com>
2022-11-02 14:26:53 +0100

    PizzaControllerTest Objectmapper dependency added, Main updated GET_ToReceiveAllPizzasSuccessfully test green

--- src/main/java/org/ec/Main.java
+++ src/main/java/org/ec/Main.java
@@ -1,11 +1,52 @@
 package org.ec;
 
 import io.javalin.Javalin;
+import org.ec.Pizza.Pizza;
+import org.ec.Pizza.PizzaController;
+import org.ec.Pizza.PizzaRepository;
+import org.ec.Pizza.PizzaService;
+
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
 	public static void main(String[] args) {
+		PizzaController pizzaController = new PizzaController(new PizzaService(new PizzaRepository() {
+			List<Pizza> pizzas = List.of(new Pizza(1, "VESUVIO", 100, 1), new Pizza(2, "MARGARITA", 90, 2));
+			@Override
+			public List<Pizza> findAll() {
+				return pizzas;
+			}
+
+			@Override
+			public void add(Pizza pizza) {
+
+			}
+
+			@Override
+			public void deleteById(int id) {
+
+			}
+
+			@Override
+			public Optional<Pizza> findById(int id) {
+				return Optional.empty();
+			}
+
+			@Override
+			public Pizza save(Pizza pizza) {
+				return null;
+			}
+
+			@Override
+			public Boolean pizzaNameExists(String pizzaName) {
+				return null;
+			}
+		}));
+
+
 		Javalin App = Javalin.create()
-				.get("/", ctx -> ctx.result("Hämta en lista på alla pizzor"))
+				.get("/", pizzaController.fetchAllPizzas())
 				.post("/", ctx -> ctx.result("Skapa en ny pizza"))
 				.delete("/{id}", ctx -> ctx.result("Radera en specifik pizza baserat på id: " + ctx.pathParam("id")))
 				.get("/groups", ctx -> ctx.result("Hämta alla pizzagrupper"))
--- src/test/java/org/ec/Pizza/PizzaControllerTest.java
+++ src/test/java/org/ec/Pizza/PizzaControllerTest.java
@@ -1,5 +1,6 @@
 package org.ec.Pizza;
 
+import io.javalin.http.Context;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,11 +9,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PizzaControllerTest {
+
+	private final Context ctx = mock(Context.class);
 
 	@Mock
 	private PizzaService pizzaService;
@@ -24,12 +26,12 @@ public class PizzaControllerTest {
 	}
 
 	@Test
-	void GET_ToReceiveAllPizzasSuccessfully() {
+	void GET_ToReceiveAllPizzasSuccessfully() throws Exception {
 		//given
 		List<Pizza> pizzas = List.of(new Pizza(1, "VESUVIO", 100, 1), new Pizza(2, "CAPRICIOSSA", 100, 1), new Pizza(3, "MARGARITA", 100, 1));
 		when(pizzaService.getAllPizzas()).thenReturn(pizzas);
 		//when
-		classUnderTest.fetchAllPizzas();
+		classUnderTest.fetchAllPizzas().handle(ctx);
 		//then
 		verify(ctx).json(pizzas);
 
